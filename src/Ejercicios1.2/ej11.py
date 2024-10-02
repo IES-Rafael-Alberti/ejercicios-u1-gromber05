8ce0f03b795eaeb10451c8b8073674843e8076be
gromber <rbgonzalo@iesrafaelalberti.es>
2024-10-03 00:35:49 +0200

    Funciones terminadas

--- src/Ejercicios1.2/ej11.py
+++ src/Ejercicios1.2/ej11.py
@@ -4,10 +4,10 @@ num = int(input('Introduzca un número: '))
 # Detectamos si el número introducido no es negativo
 if num > 0:
     # Si el número introducido no es negativo hacemos las operaciones y las imprimimos.
-    o1 = num + 1
-    o2 = num * o1
-    o3 = o2 / 2
-    print('El resultado es: ' + str(o3))
+    num = num + 1
+    num = num * num
+    num = num / 2
+    print('El resultado es: ' + str(num))
 else:
     # Si el número es negativo, te salta el error.
     print('El número no puede ser negativo')
