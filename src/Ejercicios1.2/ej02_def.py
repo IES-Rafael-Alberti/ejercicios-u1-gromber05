3bb481fd9df1d6e52314530eab2b5f82a987bfda
gromber05 <rbgonzalo@iesrafaelalberti.es>
2024-10-03 13:30:26 +0200

    Comentarioas

--- src/Ejercicios1.2/ej02_def.py
+++ src/Ejercicios1.2/ej02_def.py
@@ -1,11 +1,3 @@
-# Preguntamos cuántas horas hemos realizado
-# horas = int(input("¿Cuántas horas has realizado?: \n"))
-# Preguntamos cuánto cuesta cada hora de trabajo
-# coste = int(input("Coste por hora del trabajo: \n"))
-# Calculamos el coste final de todas las horas
-# total = str(horas * coste)
-# Imprimimos ese cálculo
-# print("Importe total: " + total) 
 
 def horario(horas, coste, total):
     return f"Importe total: {total} €"
