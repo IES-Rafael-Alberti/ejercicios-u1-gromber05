aed6f5a52409eeedbb4332394b29c940b2192c92
gromber05 <rbgonzalo@iesrafaelalberti.es>
2024-10-16 19:41:11 +0200

    Numeros aleatorios terminadi

--- src/numAleatorio.py
+++ src/numAleatorio.py
@@ -1,17 +1,15 @@
 import random
 import math
 
-contador = 0
 print(f'Bienvenido, solo tienes 5 intentos para averiguar el número, suerte')
 print(f'Intento: 0')
 
-def numAleatorio():
 
+def numAleatorio() -> int:
     numA = random.randint(0, 100)
     return numA
 
 def comprobarAleatorio(numA: int, numB: int) -> bool:
-
     if numA == numB:
         return True
     if numA != numB:
@@ -20,11 +18,11 @@ def comprobarAleatorio(numA: int, numB: int) -> bool:
 def diferencia(numA: int, numB:int) -> int:
 
     i = abs(numA - numB)
-
+    ale = ''
     if i > 75:
         ale = "Te congelas"
     elif i > 50:
-        ale = "Un poqutio de fresco"
+        ale = "Un poquito de fresco"
     elif i > 30:
         ale = "Templaito"
     elif i > 20:
@@ -33,23 +31,17 @@ def diferencia(numA: int, numB:int) -> int:
         ale = "Hace calorcillo"
     elif i > 5:
         ale = "TE QUEMAS"
-    
-    pose = posi(numA, numB)
-    print(f'{ale} {pose}')
-    
-def intentos():
-
-    global contador
-    contador = contador + 1
-    print(f'Intento: {contador}')
+    elif i >= 0:
+        ale = "TE ESTÁS QUEMANDO"
+    return ale
+  
 
 def posi(numA: int, numB: int) -> bool:
 
     if numA > numB:
-        return True
-    elif numA < numB:
         return False
-
+    elif numA < numB:
+        return True
 
 def pedirNumero() -> int:
 
@@ -68,38 +60,38 @@ def comprobarNum(num: str) -> int:
         return num
     else:
         print('**ERROR** El número introducido debe ser un número')
-    
-    
-def main():
-    
-    numAdivinar = numAleatorio()
+
+def final(numAdivinar: int):
+
     numUsuario = pedirNumero()
-
-    comprobarAleatorio = False
-    global contador
-
-    if comprobarAleatorio == False:
-        for contador in range(0,4):
-
-            diferencia = diferencia(numUsuario, numAdivinar)
-
-            posi = posi(numUsuario, numAdivinar)
-            if posi() == True:
-                posi = '>'
-            elif posi() == False:
-                posi = '<'
-            
-            print(f'{posi} {diferencia}')
-
-            numUsuario = pedirNumero()
+    comprobacion = comprobarAleatorio(numUsuario, numAdivinar)
+    global Configintentos
     
+    if comprobacion is False:  
+
+        for contador in range(0, 10):
+            diferen = diferencia(numUsuario, numAdivinar)
+
+            pos = posi(numUsuario, numAdivinar)
+            if pos == True:
+                pos2 = '>'
+            elif pos == False:
+                pos2 = '<'
+            
+            print(f'{pos2} {diferen}')
+
+            final(numAdivinar)
+
         else: 
             print('Mala suerte, te quedaste sin intentos:')
     else:
         print('Felicidades!')
-"""
-intentos()
-        diferencia(numA, numB)
-"""
+        exit()
+    
+def main():
+
+    numAdivinar = numAleatorio()
+    final(numAdivinar)
+
 if __name__ == '__main__':
     main()
